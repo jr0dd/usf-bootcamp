a6b8a6e42a34cb7ea994f1dd3f65cc06c7cad6c2
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-04-04 13:40:16 -0400

    add timers assignment

---
+++ timers.js
@@ -0,0 +1,24 @@
+function countDown (num) {
+  const counter = setInterval(() => {
+    if (num <= 0) {
+      console.log('DONE')
+      clearInterval(counter)
+    } else {
+      console.log(num--)
+    }
+  }, 1000)
+}
+
+function randomGame () {
+  let num = 0
+  let count = 0
+  const counter = setInterval(() => {
+    num = Math.random()
+    count++
+    console.log(num)
+    if (num > 0.75) {
+      console.log(`It took ${count} tries`)
+      clearInterval(counter)
+    }
+  }, 1000)
+}
