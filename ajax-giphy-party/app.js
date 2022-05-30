bd72c36c7271e37099e14e203fc76b308c41f640
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-05-30 18:04:35 -0400

    ajax giphy party

---
+++ ajax-giphy-party/app.js
@@ -0,0 +1,33 @@
+$(() => {
+  $('#search-form').on('submit', async (e) => {
+    e.preventDefault()
+
+    const imgUrl = await giphySearch($('#query').val())
+
+    // create gif
+    $('#gif-container').append(`
+      <div class="gif">
+        <img src=${imgUrl}></img>
+      </div>
+    `)
+  
+    $('#search-form').trigger('reset')
+    $('#query').trigger('focus')
+  })
+
+  $('#remove').on('click', () => {
+    $('#gif-container').empty()
+  })
+})
+
+const giphySearch = async (str) => {
+  const res = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: str,
+      api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'
+    }
+  })
+  const count = res.data.data.length
+  const idx = Math.floor(Math.random() * count)
+  return res.data.data[idx].images.original.url
+}
