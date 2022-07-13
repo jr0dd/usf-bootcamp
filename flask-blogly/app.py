0e13d6d6c52ef35518ac46587265d2cf2d2801bd
jr0dd <285797+jr0dd@users.noreply.github.com>
2022-07-13 14:55:34 -0400

    add flask-blogly initial files

---
+++ flask-blogly/app.py
@@ -0,0 +1,12 @@
+"""Blogly application."""
+
+from flask import Flask, redirect, request, render_template
+from models import db, connect_db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
