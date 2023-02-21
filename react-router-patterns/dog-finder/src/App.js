02fe2c2408711e695201a1019dc210c2c3534b29
jr0dd <285797+jr0dd@users.noreply.github.com>
2023-02-21 16:20:55 -0500

    add react-router-patterns

---
+++ react-router-patterns/dog-finder/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <DogList />
+    </div>
+  )
+}
+
+App.defaultProps = {
+  dogs: [
+    {
+      name: "Whiskey",
+      age: 5,
+      src: whiskey,
+      facts: [
+        "Whiskey loves eating popcorn.",
+        "Whiskey is a terrible guard dog.",
+        "Whiskey wants to cuddle with you!"
+      ]
+    },
+    {
+      name: "Duke",
+      age: 3,
+      src: duke,
+      facts: [
+        "Duke believes that ball is life.",
+        "Duke likes snow.",
+        "Duke enjoys pawing other dogs."
+      ]
+    },
+    {
+      name: "Perry",
+      age: 4,
+      src: perry,
+      facts: [
+        "Perry loves all humans.",
+        "Perry demolishes all snacks.",
+        "Perry hates the rain."
+      ]
+    },
+    {
+      name: "Tubby",
+      age: 4,
+      src: tubby,
+      facts: [
+        "Tubby is really stupid.",
+        "Tubby does not like walks.",
+        "Angelina used to hate Tubby, but claims not to anymore."
+      ]
+    }
+  ]
+}
+
+export default App
