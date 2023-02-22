9a440d04eefc4e36e04696b0a037427e849c28c7
jr0dd <285797+jr0dd@users.noreply.github.com>
2023-02-22 12:57:36 -0500

    working dog finder

--- react-router-patterns/dog-finder/src/DogDetails.js
+++ react-router-patterns/dog-finder/src/DogDetails.js
@@ -3,18 +3,24 @@ import { useParams } from 'react-router-dom'
 import './DogDetails.css'
 
 const DogDetails = ({ dogs }) => {
-    const { dogName } = useParams()
-    const { age, facts, name, src } = dogs.filter(dog => dog.name.toLowerCase() === dogName.toLowerCase())[0] 
+  const { name } = useParams()
+  const data = dogs.find(dog => dog.name.toLowerCase() === name)
 
+  if (!data) {
     return (
-      <div className='DogDetails'>
-        <h4>{name}</h4>
-        <img src={src} alt={name.toLowerCase()}></img>
-        <div><b>Age:</b>{age}</div>
-        <div><b>Facts:</b></div>
-        <div>{facts.join(' ')}</div>
-      </div>
+      <p>Loading</p>
     )
+  }
+
+  return (
+    <div className='DogDetails'>
+      <h4>{name}</h4>
+      <img src={data.src} alt={data.name}></img>
+      <div><b>Age:</b>{data.age}</div>
+      <div><b>Facts:</b></div>
+      <div>{data.facts.join(' ')}</div>
+    </div>
+  )
 }
 
 export default DogDetails
--- react-router-patterns/dog-finder/src/Doglist.js
+++ react-router-patterns/dog-finder/src/Doglist.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './DogList.css'
 
 const DogList = ({ dogs }) => {
   return (
     <div className='DogList'>
-      <h2>Dog List:</h2>
       {dogs.map(dog => (
-        <div key={dog.name.toLowerCase()}>
-          <Link to={`/dogs/${dog.name.toLowerCase()}`}>
-            {dog.name}
-          </Link>
-        </div>
+        <NavLink key={dog.name.toLowerCase()} to={`/dogs/${dog.name.toLowerCase()}`}>
+          {dog.name}
+        </NavLink>
       ))}
     </div>
   )
