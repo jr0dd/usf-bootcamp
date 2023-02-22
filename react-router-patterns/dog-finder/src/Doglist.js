8147901bffbaec6a2e5ebbccb93967670c0725af
jr0dd <285797+jr0dd@users.noreply.github.com>
2023-02-22 13:06:32 -0500

    css tweaks

--- react-router-patterns/dog-finder/src/Doglist.js
+++ react-router-patterns/dog-finder/src/Doglist.js
@@ -5,6 +5,7 @@ import './DogList.css'
 const DogList = ({ dogs }) => {
   return (
     <div className='DogList'>
+      <h3>Dog List:</h3>
       {dogs.map(dog => (
         <NavLink key={dog.name.toLowerCase()} to={`/dogs/${dog.name.toLowerCase()}`}>
           {dog.name}
