4bf27f723d538f0c02451de54ccb5949b46bc8ab
jr0dd <285797+jr0dd@users.noreply.github.com>
2023-04-12 11:38:34 -0400

    add dsa exercises

---
+++ dsa-recursion/recursion.js
@@ -0,0 +1,59 @@
+/** product: calculate the product of an array of numbers. */
+
+function product(nums) {
+
+}
+
+/** longest: return the length of the longest word in an array of words. */
+
+function longest(words) {
+
+}
+
+/** everyOther: return a string with every other letter. */
+
+function everyOther(str) {
+
+}
+
+/** isPalindrome: checks whether a string is a palindrome or not. */
+
+function isPalindrome(str) {
+
+}
+
+/** findIndex: return the index of val in arr (or -1 if val is not present). */
+
+function findIndex(arr, val) {
+
+}
+
+/** revString: return a copy of a string, but in reverse. */
+
+function revString(str) {
+
+}
+
+/** gatherStrings: given an object, return an array of all of the string values. */
+
+function gatherStrings(obj) {
+
+}
+
+/** binarySearch: given a sorted array of numbers, and a value,
+ * return the index of that value (or -1 if val is not present). */
+
+function binarySearch(arr, val) {
+
+}
+
+module.exports = {
+  product,
+  longest,
+  everyOther,
+  isPalindrome,
+  findIndex,
+  revString,
+  gatherStrings,
+  binarySearch
+};
