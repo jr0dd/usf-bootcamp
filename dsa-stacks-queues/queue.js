c126407b46f15bac2ac625dc7ccfb49057ac64ee
jr0dd <285797+jr0dd@users.noreply.github.com>
2023-04-12 11:55:58 -0400

    add stacks and queues

--- dsa-stacks-queues/queue.js
+++ dsa-stacks-queues/queue.js
@@ -1,9 +1,9 @@
 /** Node: node for a queue. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+  constructor (val) {
+    this.val = val
+    this.next = null
   }
 }
 
@@ -11,36 +11,56 @@ class Node {
  *  remove from the front or add to the back. */
 
 class Queue {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
+  constructor () {
+    this.first = null
+    this.last = null
+    this.size = 0
   }
 
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
-  enqueue(val) {
+  enqueue (val) {
+    const node = new Node(val)
 
+    if (!this.first) {
+      this.first = node
+      this.last = node
+    } else {
+      this.last.next = node
+      this.last = node
+    }
+
+    this.size++
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
-  dequeue() {
+  dequeue () {
+    if (!this.first) throw new Error('Queue is empty')
 
+    const temp = this.first
+    if (this.first === this.last) {
+      this.last = null
+    }
+
+    this.first = this.first.next
+    this.size--
+
+    return temp.val
   }
 
   /** peek(): return the value of the first node in the queue. */
 
-  peek() {
-
+  peek () {
+    return this.first.val
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
-  isEmpty() {
-
+  isEmpty () {
+    return this.size === 0
   }
 }
 
-module.exports = Queue;
+module.exports = Queue
--- dsa-stacks-queues/stack.js
+++ dsa-stacks-queues/stack.js
@@ -1,9 +1,9 @@
 /** Node: node for a stack. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+  constructor (val) {
+    this.val = val
+    this.next = null
   }
 }
 
@@ -11,36 +11,58 @@ class Node {
  *  remove from the top or add to the top. */
 
 class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
+  constructor () {
+    this.first = null
+    this.last = null
+    this.size = 0
   }
 
   /** push(val): add new value to end of the stack. Returns undefined. */
 
-  push(val) {
+  push (val) {
+    const node = new Node(val)
 
+    if (!this.first) {
+      this.first = node
+      this.last = node
+    } else {
+      const tmp = this.first
+      this.first = node
+      this.first.next = tmp
+    }
+
+    this.size++
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
-  pop() {
+  pop () {
+    if (!this.first) throw new Error('Stack is empty')
 
+    const temp = this.first
+
+    if (this.first === this.last) {
+      this.last = null
+    }
+
+    this.first = this.first.next
+    this.size--
+
+    return temp.val
   }
 
   /** peek(): return the value of the first node in the stack. */
 
-  peek() {
-
+  peek () {
+    return this.first.val
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
-  isEmpty() {
-
+  isEmpty () {
+    return this.size === 0
   }
 }
 
-module.exports = Stack;
+module.exports = Stack
