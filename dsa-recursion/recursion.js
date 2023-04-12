47e8a8b8e07511f6bfabcc4fd62684ee2f965bb2
jr0dd <285797+jr0dd@users.noreply.github.com>
2023-04-12 12:32:16 -0400

    add recursion

--- dsa-recursion/recursion.js
+++ dsa-recursion/recursion.js
@@ -1,50 +1,62 @@
 /** product: calculate the product of an array of numbers. */
 
-function product(nums) {
-
+const product = (nums, idx = 0) => {
+  if (idx === nums.length) return 1
+  return nums[idx] * product(nums, idx + 1)
 }
 
 /** longest: return the length of the longest word in an array of words. */
 
-function longest(words) {
-
+const longest = (words, idx = 0, cur = 0) => {
+  if (idx === words.length) return cur
+  cur = Math.max(words[idx].length, cur)
+  return longest(words, idx + 1, cur)
 }
 
 /** everyOther: return a string with every other letter. */
 
-function everyOther(str) {
-
+const everyOther = (str, idx = 0, newStr = '') => {
+  if (idx >= str.length) return newStr
+  newStr += str[idx]
+  return everyOther(str, idx + 2, newStr)
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
-function isPalindrome(str) {
-
+const isPalindrome = (str, idx = 0) => {
+  const left = idx
+  const right = str.length - idx - 1
+  if (left >= right) return true
+  if (str[left] !== str[right]) return false
+  return isPalindrome(str, idx + 1)
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {
-
+const findIndex = (arr, val, idx = 0) => {
+  if (idx === arr.length) return -1
+  if (arr[idx] === val) return idx
+  return findIndex(arr, val, idx + 1)
 }
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {
-
+const revString = (str, idx = 0, newStr = '') => {
+  if (newStr.length === str.length) return newStr
+  newStr += str[str.length - 1 - idx]
+  return revString(str, idx + 1, newStr)
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {
+const gatherStrings = (obj) => {
+  const arr = []
 
-}
-
-/** binarySearch: given a sorted array of numbers, and a value,
- * return the index of that value (or -1 if val is not present). */
-
-function binarySearch(arr, val) {
-
+  for (const key in obj) {
+    if (typeof obj[key] === 'string') arr.push(obj[key])
+    if (typeof obj[key] === 'object') arr.push(...gatherStrings(obj[key]))
+  }
+  return arr
 }
 
 module.exports = {
@@ -54,6 +66,5 @@ module.exports = {
   isPalindrome,
   findIndex,
   revString,
-  gatherStrings,
-  binarySearch
-};
+  gatherStrings
+}
